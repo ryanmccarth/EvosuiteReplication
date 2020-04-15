27ec71702e7c343d86f8b62c948831c643566c98
ryanmccarth <ryanfmccarthy30@gmail.com>
2020-04-15 15:16:25 -0400

    Updated script to work as intended on simple java maven based project

--- run_evosuite.sh
+++ run_evosuite.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
 
-#a shell script which downloads a project via url and runs evosuite against it
+# a shell script which downloads a project via url and runs evosuite against it
 
-#downloads project via name of zip file given via argument
+# takes desired name of parent directory and name of zipped project as args
 DIR=$1
 ZIP=$2
-EVOSUITE='java -jar evosuite.jar'
+EVOSUITE='java -jar ../../../evosuite.jar'
 
-# unzip 'ReplicationProjects/'$ZIP -d 'ReplicationProjects/'$DIR
+# unzip zip file into ReplicationProjects directory
+unzip 'ReplicationProjects/'$ZIP -d 'ReplicationProjects/'$DIR
 
-echo 'Running EvoSuite'
-$EVOSUITE -listClasses -target 'ReplicationProjects/'$DIR
+# move into unzipped project's parent directory
+cd 'ReplicationProjects/'$DIR
+# get name of unzipped directory and move into it
+PROJECT=$(ls | head -n 1)
+cd $PROJECT
 
-# $EVOSUITE -target 'ReplicationProjects/'$DIR -projectCP 'target/classes'
+#compiles the project with mvn
+mvn 'compile'
+#run evosuite against ALL newly compiled classes
+$EVOSUITE -prefix '' -projectCP 'target/classes'
+#move statistics file to results directory
+mv 'evosuite-report/statistics.csv' '../../../ReplicationResults/'$DIR'_statistics.csv'
+# $EVOSUITE -listClasses -target 'C:\Users\ryanm\OneDrive\Desktop\CS520\EvosuiteReplication\ReplicationProjects\Tutorial_Stack\Tutorial_Stack\src'
